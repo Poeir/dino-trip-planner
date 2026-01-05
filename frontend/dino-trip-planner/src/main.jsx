b6d82d284fc438c45271ad6321548a7ae6023438
Poori Charicha <phuri.c@kkumail.com>
2026-01-05 22:50:45 +0700

    create skeleton of homepage page

--- frontend/dino-trip-planner/src/main.jsx
+++ frontend/dino-trip-planner/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import HomepageApp from './pages/homepagePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HomepageApp />
   </StrictMode>,
 )
